e3167396b5f8fc7ec2c04f82d03e23f15bb44455
Tim <tim7777700000@gmail.com>
2025-10-19 23:18:03 +0800

    feat: add metrics

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { PrometheusModule } from '@willsoto/nestjs-prometheus';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    PrometheusModule.register({
+      defaultMetrics: {
+        enabled: true,
+      },
+      path: '/metrics',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
